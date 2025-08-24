fc89266d81213554501f6e4e97cadd98b2ddbab7
EC2 Default User <ec2-user@ip-172-31-81-69.ec2.internal>
2025-08-24 08:20:55 +0000

    Adding more parts of CI/CD

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -2,20 +2,3 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = var.bucket_name_tfstate
 }
-
-# BLOCKED PUBLIC ACCESS
-resource "aws_s3_bucket_public_access_block" "public-access" {
-  bucket                  = aws_s3_bucket.tfstate.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-#VERSION ENABLE
-resource "aws_s3_bucket_versioning" "version-enable-conf" {
-  bucket = aws_s3_bucket.tfstate.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
