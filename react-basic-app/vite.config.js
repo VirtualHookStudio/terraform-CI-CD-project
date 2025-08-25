155b0a367bfe8f4c5f91d9374af3df0cc2fda572
VirtualHookStudio <133015475+VirtualHookStudio@users.noreply.github.com>
2025-08-25 04:15:22 -0300

    Update vite.config.js
    add

--- react-basic-app/vite.config.js
+++ react-basic-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
+    allowedHosts: ['.elb.amazonaws.com']
     port: 80,
     host: true
   }
